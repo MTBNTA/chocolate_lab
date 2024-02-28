f29344517fd7f2ef8da6a3ebf0f463adfeb4cce9
Hannah Riley <hannah.847@btinternet.com>
2024-02-28 16:40:29 +0000

    first part of extension complete

--- src/main/java/com/bnta/chocolate/controllers/EstateController.java
+++ src/main/java/com/bnta/chocolate/controllers/EstateController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/estates")
@@ -22,6 +24,16 @@ public class EstateController {
     public ResponseEntity<List<Estate>> getAllEstates(){
         List<Estate> estates = estateService.getAllEstates();
         return new ResponseEntity<>(estates, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Estate> getEstateById(@PathVariable long id){
+
+        Optional<Estate> estate = estateService.getEstateById(id);
+        if(estate.isPresent()){
+            return new ResponseEntity<>(estate.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
 }
--- src/main/java/com/bnta/chocolate/services/EstateService.java
+++ src/main/java/com/bnta/chocolate/services/EstateService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EstateService {
@@ -20,6 +21,10 @@ public class EstateService {
 
     public List<Estate> getAllEstates(){
         return estateRepository.findAll();
+    }
+
+    public Optional<Estate> getEstateById(long id){
+        return estateRepository.findById(id);
     }
 
 }
